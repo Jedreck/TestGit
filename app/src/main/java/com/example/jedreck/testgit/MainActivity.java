3d56488673037245ec2ff51eef18406ebdea99be
Jedreck <a302720001@outlook.com>
2018-04-16 21:04:51 +0800

    加上Button和Toast

--- app/src/main/java/com/example/jedreck/testgit/MainActivity.java
+++ app/src/main/java/com/example/jedreck/testgit/MainActivity.java
@@ -2,6 +2,9 @@ package com.example.jedreck.testgit;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends Activity {
 
@@ -9,5 +12,12 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button button = (Button)findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this,R.string.Lalala,Toast.LENGTH_LONG);
+            }
+        });
     }
 }
