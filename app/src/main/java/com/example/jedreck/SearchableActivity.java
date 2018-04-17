a0424afcfda03d6be1797f2b7412d50fe76ad78a
ooxian <38432613+ooxian@users.noreply.github.com>
2018-04-17 19:39:33 +0800

    Add files via upload

---
+++ app/src/main/java/com/example/jedreck/SearchableActivity.java
@@ -0,0 +1,13 @@
+package com.example.major_search;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class SearchableActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.searchable);
+    }
+}
